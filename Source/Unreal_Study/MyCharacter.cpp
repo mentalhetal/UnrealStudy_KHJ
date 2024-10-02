0264de28310c5467dea5a6cb0e69e04d164c7304
mentalhetal <37432702+mentalhetal@users.noreply.github.com>
2024-10-02 22:44:02 +0900

    라이브코딩 해결
    라이브코딩 해결

--- Source/Unreal_Study/MyCharacter.cpp
+++ Source/Unreal_Study/MyCharacter.cpp
@@ -1,9 +1,5 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "MyCharacter.h"
 
-// Sets default values
 AMyCharacter::AMyCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
@@ -11,21 +7,18 @@ AMyCharacter::AMyCharacter()
 
 }
 
-// Called when the game starts or when spawned
 void AMyCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
 void AMyCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
-// Called to bind functionality to input
 void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
